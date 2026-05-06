1f2351e0777414d3402c9e6886b676cc1224c9a9
Ashikur Rahaman <ashikurrahaman9988@gmail.com>
2026-05-06 12:47:27 +0600

    Dashboard menu Caching Enebeled

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-     { protocol: "https", hostname: "**" }
+      { protocol: "https", hostname: "**" }
     ],
+  },
+  experimental: {
+    staleTimes: {
+      dynamic: 30,  // 30 seconds cache
+      static: 180,  // 3 minutes cache
+    },
   },
 }
 
